805b3f36d3df4e1e20cb05ed640d482e358c6c8e
Tanysha23 <tanysha.karpovich@gmail.com>
2021-03-14 22:33:24 +0300

    feat: brackets check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let array = str.split("");
+    let stack = [];
+
+    for (let i = 0; i < array.length; i++) {
+        stack.push(array[i]);
+        for (let j = 0; j < bracketsConfig.length; j++){
+            if (bracketsConfig[j][0] === stack[stack.length-2]
+                && bracketsConfig[j][1] === stack[stack.length-1]){
+                stack.pop();
+                stack.pop();
+            }
+        }
+    }
+    return !stack.length;
 }
+
